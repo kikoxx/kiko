19b85d54891b95fa6bea500fd1f3a1f21d4e7760
kiko <kikour@gmail.com>
2025-01-06 21:54:16 +0800

    .

--- install.sh
+++ install.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Function to prompt user to continue or exit
-prompt_user() {
-    while true; do
-        read -p "Do you want to continue with the next installation? (y/n): " choice
-        case $choice in
-            [Yy]* ) return 0;;  # Continue with the next script
-            [Nn]* ) echo "Exiting the installation."; exit 0;;  # Exit if user chooses 'no'
-            * ) echo "Please answer with 'y' or 'n': ";;  # Ask again if user enters an invalid response
-        esac
-    done
-}
-
 # Function to run install scripts asynchronously
 run_installation() {
     local dir=$1
@@ -29,15 +17,12 @@ echo "[+] Starting installations..."
 
 # Run alacritty install
 run_installation "alacritty"
-prompt_user
 
 # Run vim install
 run_installation "vim"
-prompt_user
 
 # Run tmux install
 run_installation "tmux"
-prompt_user
 
 # Run vim install
 run_installation "mount"
