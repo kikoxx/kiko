8bec76098ff11fb80d6b6ff80d577ecb7804d9e6
kikour <143394515+kikoxx@users.noreply.github.com>
2026-09-18 20:39:47 +0800

    fix

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-vim="[+] Setting up vim configs..."
+# Use a clean variable name instead of 'vim'
+msg="[+] Setting up vim configs..."
 
-for ((i=0; i<${#vim}; i++)); do
-    echo -n "${vim:$i:1}"
+for ((i=0; i<${#msg}; i++)); do
+    echo -n "${msg:$i:1}"
     sleep 0.05
 done
 echo
@@ -21,39 +22,38 @@ fi
 # Add vim-plug configuration to .vimrc if not already present
 if ! grep -q "call plug#begin" ~/.vimrc; then
     echo "Adding vim-plug configuration to .vimrc..."
-
-    cat <<EOL >> ~/.vimrc
+    cat << 'EOL' >> ~/.vimrc
 call plug#begin('~/.vim/plugged')
 Plug 'audibleblink/hackthebox.vim'
 call plug#end()
 EOL
 fi
 
-# Install plugins in vim
+# Install plugins headlessly (won't freeze the script)
 echo "Installing plugins with vim..."
-vim +PlugInstall +qall
+vim -es -u ~/.vimrc +PlugInstall +qall
 
-# If it doesn't work do it manually with the following
+# Manual fallback instructions
 manual="Do it manually if it doesn't work:"
 for ((i=0; i<${#manual}; i++)); do
     echo -n "${manual:$i:1}"
     sleep 0.05
 done
 echo
-echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 echo
-echo nano .vimrc
+echo "nano ~/.vimrc"
 echo "call plug#begin('~/.vim/plugged')"
 echo "Plug 'audibleblink/hackthebox.vim'"
 echo "call plug#end()"
 echo
 echo "vim"
 echo ":PlugInstall"
-
+echo
 
 exit_txt="Exiting script now..."
 for ((i=0; i<${#exit_txt}; i++)); do
     echo -n "${exit_txt:$i:1}"
     sleep 0.05
 done
+echo
